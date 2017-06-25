1efebed27288fbc2a68c9c7bfe7168d68e2b4216
Bryan Lockward <bmlockward3@verizon.net>
2017-06-24 20:31:07 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+
+  $('#hide').click(
+    function(){
+      $("#hidden").hide();
+    });
+
+  $("#show").click(function(){
+    $("#shown").show();
+  });
+
+  $("#toggle").click(function(){
+    $("#tog").toggle();
+  });
+
+  $("#slideUp").click(function(){
+    $("#slide").slideUp();
+  });
+
+  $("#slideDown").click(function(){
+    $("#slide").slideDown();
+  });
+
+  $("#slideTog").click(function(){
+    $("#slidetoggle").slideToggle();
+  });
+
+  $("#fadeIn").click(function(){
+    $("#fade").fadeIn();
+  });
+
+  $("#fadeOut").click(function(){
+    $("#fade").fadeOut();
+  });
+
+  $("#addclass").click(function(){
+    $('.classchange').addClass('newclass')
+  });
+
+  $('h2').hover(
+    function(){
+      $(this).css('color','#AEEE00');
+    },
+    function(){
+      $(this).css('color','white');
+    });
+
+    $('#para').before("Hello");
+    $('#para').after("ITS GREAT");
+
+  });
